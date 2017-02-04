9b51f6d0f396ca8026512291b48857b3bfc71e76
Shell Bryson <shell@shellbryson.com>
2017-02-04 18:14:59 +0000

    Fixes desktop search position. Adds search toggle. Adds search to menu.

--- components/navigation/assets/scripts/navigation.main.js
+++ components/navigation/assets/scripts/navigation.main.js
@@ -3,19 +3,20 @@ define(function (require) {
 
   var $ = require('jquery');
   var $nav = $('.js-navigation');
+  var $search = $('.js-search');
   var $toggleMenu = $('.js-toggleMenu');
   var $toggleSearch = $('.js-toggleSearch');
 
   function initNavigation() {
-
     $toggleMenu.on('click', function (e) {
       e.preventDefault();
       $nav.toggleClass('su-navigation__menu--show');
+      $search.toggleClass('su-navigation-search--show');
     });
 
     $toggleSearch.on('click', function (e) {
       e.preventDefault();
-      $nav.toggleClass('su-search--show');
+      $search.toggleClass('su-navigation-search--show');
     });
   }
 
