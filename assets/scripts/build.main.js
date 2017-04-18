65405611740a561a56bc7ca2682c0394064ac0ea
Shell Bryson <shell@shellbryson.com>
2017-04-18 15:48:43 +0100

    Adds scaffolding for Homepage based on Sketch/PhotoShop designs

--- assets/scripts/build.main.js
+++ assets/scripts/build.main.js
@@ -9,7 +9,7 @@ define(function (require) {
       location.hostname === '127.0.0.1' ||
       location.hostname === 'sheru.local'
   ) {
-    $('head').append('<link href="https://fonts.googleapis.com/css?family=Overpass" rel="stylesheet">');
+    $('head').append('<link href="https://fonts.googleapis.com/css?family=Overpass:300,400,600" rel="stylesheet">');
     $('head').append('<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">');
   }
 
