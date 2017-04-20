04fe7edbf3be79a2a8d6fbe404b74351f8aa6529
Shell Bryson <shell@shellbryson.com>
2017-04-20 17:13:30 +0100

    Adds functionality for creating and cloning of SVG elements in JS

--- components/project-spinner/assets/scripts/project-spinner.main.js
+++ components/project-spinner/assets/scripts/project-spinner.main.js
@@ -2,10 +2,31 @@ define(function (require) {
   'use strict';
 
   var $ = require('jquery');
-  var $spinner = $('.su-project-spinner__pulse');
   var TweenMax = require('TweenMax');
+  var $spinner = $('.su-project-spinner__pulse');
 
   var $spinnerOuter = $("#pulse-outer");
+
+  function cloneSVG(id, newID, x, y) {
+    var node = document.getElementById(id);
+    var newNode = node.cloneNode("true");
+    newNode.id = newID;
+    newNode.setAttributeNS(null, "cx", x);
+    newNode.setAttributeNS(null, "cy", y);
+    document.getElementById("Home").appendChild(newNode);
+  }
+
+  function createProject(id, x, y) {
+    var project = document.createElementNS("http://www.w3.org/2000/svg","circle");
+    project.setAttributeNS(null, "id", id);
+    project.setAttributeNS(null, "cx", x);
+    project.setAttributeNS(null, "cy", y);
+    project.setAttributeNS(null, "r", 20);
+    project.setAttributeNS(null, "fill","black");
+    project.setAttributeNS(null, "stroke","none");
+    project.setAttributeNS(null, "class","spinner__project");
+    document.getElementById("Home").appendChild(project);
+  }
 
   function initSpinner() {
 
@@ -29,6 +50,11 @@ define(function (require) {
         );
     });
 
+    createProject("test1", 200, 200);
+
+    cloneSVG("test1", "test2", 300, 300);
+    cloneSVG("test1", "test3", 300, 340);
+    cloneSVG("test1", "test4", 300, 380);
   }
 
   initSpinner();
