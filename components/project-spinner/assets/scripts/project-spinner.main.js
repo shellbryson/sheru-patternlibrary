40208004a30801bb7b9653f634da9007f0237703
Shell Bryson <shell@shellbryson.com>
2017-04-24 14:02:32 +0100

    Spinner: Minor ES6 conversion. Now passes lint.

--- components/project-spinner/assets/scripts/project-spinner.main.js
+++ components/project-spinner/assets/scripts/project-spinner.main.js
@@ -3,49 +3,47 @@ define(function (require) {
 
   const $ = require('jquery');
   const TweenMax = require('TweenMax');
-  var $btnTogglePause = $(".js-pause");
-  var $spinner = $('.su-project-spinner__pulse');
-  var $spinnerInner = $("#pulse-inner");
-  var $spinnerOuter = $("#pulse-outer");
-  var $spinnerQuad = $("#pulse-quad");
-  var strings = {
+  const $btnTogglePause = $('.js-pause');
+  const $spinner = $('.su-project-spinner__pulse');
+  const $spinnerInner = $('#pulse-inner');
+  const $spinnerOuter = $('#pulse-outer');
+  const $spinnerQuad = $('#pulse-quad');
+  const strings = {
     buttonPause: {
-      pause: "Pause animation",
-      resume: "Resume animation"
+      pause: 'Pause animation',
+      resume: 'Resume animation'
     }
-  }
-  var state = {
+  };
+  const state = {
     animated: true
-  }
-
-  // test
+  };
 
   // Creates a clone of svg element [id] as [newID]
   function cloneSVG(id, newID, x, y, parentID) {
-    var node = document.getElementById(id);
-    var newNode = node.cloneNode("true");
+    const node = document.getElementById(id);
+    const newNode = node.cloneNode('true');
     newNode.id = newID;
-    newNode.setAttributeNS(null, "cx", x);
-    newNode.setAttributeNS(null, "cy", y);
+    newNode.setAttributeNS(null, 'cx', x);
+    newNode.setAttributeNS(null, 'cy', y);
     document.getElementById(parentID).appendChild(newNode);
   }
 
   // Creates a new svg element [id] attached to [parentID]
   function createSVGElement(id, cssClass, x, y, parentID) {
-    var svg = document.createElementNS("http://www.w3.org/2000/svg","circle");
-    svg.setAttributeNS(null, "id", id);
-    svg.setAttributeNS(null, "cx", x);
-    svg.setAttributeNS(null, "cy", y);
-    svg.setAttributeNS(null, "r", 20);
-    svg.setAttributeNS(null, "fill","black");
-    svg.setAttributeNS(null, "stroke","none");
-    svg.setAttributeNS(null, "class", cssClass);
+    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+    svg.setAttributeNS(null, 'id', id);
+    svg.setAttributeNS(null, 'cx', x);
+    svg.setAttributeNS(null, 'cy', y);
+    svg.setAttributeNS(null, 'r', 20);
+    svg.setAttributeNS(null, 'fill', 'black');
+    svg.setAttributeNS(null, 'stroke', 'none');
+    svg.setAttributeNS(null, 'class', cssClass);
     document.getElementById(parentID).appendChild(svg);
   }
 
   // Creates a new project
   function createProject(id, x, y) {
-    createSVGElement(id, "spinner__project", x, y, "Spinner")
+    createSVGElement(id, 'spinner__project', x, y, 'Spinner');
   }
 
   function initSpinner() {
@@ -54,17 +52,17 @@ define(function (require) {
 
     $btnTogglePause.text( strings.buttonPause.pause );
 
-    $btnTogglePause.on("click", function() {
-      if (state.animated) {
-        state.animated = false;
-        $(this).text(strings.buttonPause.resume);
+    $btnTogglePause.on('click', function () {
+      if ( state.animated ) {
+        $( this ).text( strings.buttonPause.resume );
         animOuterRing.pause();
         animQuad.pause();
+        state.animated = false;
       } else {
-        state.animated = true;
-        $(this).text(strings.buttonPause.pause);
+        $( this ).text( strings.buttonPause.pause );
         animOuterRing.play();
         animQuad.play();
+        state.animated = true;
       }
     });
 
@@ -72,41 +70,41 @@ define(function (require) {
 
     var animOuterRing = new TweenMax.to(
       $spinnerOuter, 15, {
-        rotation: 360, transformOrigin:"center center", ease: Linear.easeNone, repeat:-1
-      },{
+        rotation: 360, transformOrigin: 'center center', ease: Linear.easeNone, repeat:-1
+      }, {
         timeScale: 1
       }
     );
 
     var animQuad = new TweenMax.to(
       $spinnerQuad, 60, {
-        rotation: 360, transformOrigin:"center center", ease: Linear.easeNone, repeat:-1
-      },{
+        rotation: 360, transformOrigin: 'center center', ease: Linear.easeNone, repeat:-1
+      }, {
         timeScale: 1
       }
     );
 
     // Interactions
 
-    $spinner.on("mouseover", function() {
+    $spinner.on('mouseover', () => {
         TweenLite.to(
-          animOuterRing, 1, {timeScale: 10}
+          animOuterRing, 1, { timeScale: 10 }
         );
     });
 
-    $spinner.on("mouseout", function() {
+    $spinner.on('mouseout', () => {
         TweenLite.to(
-          animOuterRing, 1, {timeScale: 1}
+          animOuterRing, 1, { timeScale: 1 }
         );
     });
 
     // Creation
 
-    createProject("test1", 200, 200);
+    createProject( 'test1', 200, 200 );
 
-    cloneSVG("test1", "test2", 300, 300, "Spinner");
-    cloneSVG("test1", "test3", 300, 340, "Spinner");
-    cloneSVG("test1", "test4", 300, 380, "Spinner");
+    cloneSVG( 'test1', 'test2', 300, 300, 'Spinner' );
+    cloneSVG( 'test1', 'test3', 300, 340, 'Spinner' );
+    cloneSVG( 'test1', 'test4', 300, 380, 'Spinner' );
   }
 
   initSpinner();
